2ca74a6955a56fcca0f7c7444d6ef25774bccd70
toshi0607 <s.toshi0607@gmail.com>
2018-06-24 12:54:48 +0900

    :tada: to selecting delivery time page

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/sclevine/agouti"
 	"github.com/prometheus/common/log"
-	"time"
 	"os"
 )
 
@@ -21,6 +20,7 @@ func main() {
 		log.Fatalf("Failed to open page:%v", err)
 	}
 
+	// ログイン
 	if err := page.Navigate("https://www.kakuyasu.co.jp/ec/common/CSfLogin.jsp?transfer=../member/CPmPersonalShop_001.jsp"); err != nil {
 		log.Fatalf("Failed to navigate:%v", err)
 	}
@@ -30,6 +30,50 @@ func main() {
 	page.Find("#bodyContainer > form > div.login_left > div > div.heightLine-group99 > div > dl:nth-child(1) > dd > ul > li > input").Fill(id)
 	page.Find("#bodyContainer > form > div.login_left > div > div.heightLine-group99 > div > dl:nth-child(2) > dd > ul > li > input").Fill(pass)
 	page.Find("#bodyContainer > form > div.login_left > div > div.loginbtn > input").Click()
-	time.Sleep(3 * time.Second)
 	page.Screenshot("log/02.png")
+
+	// 定番注文
+	page.FindByID("mylink5").Click()
+	page.Screenshot("log/03.png")
+
+	// カートに追加
+	page.Find("#bodyContainer > table > tbody > tr:nth-child(9) > td:nth-child(7) > input:nth-child(1)").Click()
+	page.Screenshot("log/04.png")
+
+	// ご注文手続きへ進む
+	page.FindByID("goOrder").Click()
+	page.Screenshot("log/05.png")
+
+	// ログイン情報再入力
+	page.Find("#bodyContainer > form > div.login_left > div > div.heightLine-group99 > div > dl:nth-child(1) > dd > ul > li > input").Fill(id)
+	page.Find("#bodyContainer > form > div.login_left > div > div.heightLine-group99 > div > dl:nth-child(2) > dd > ul > li > input").Fill(pass)
+	page.Find("#bodyContainer > form > div.login_left > div > div.loginbtn > input").Click()
+	page.Screenshot("log/06.png")
+
+	// お届け方法・希望日を指定する
+	price, _ := page.Find("#COdorInfo_tb2 > div.step1Area01 > div:nth-child(6) > dl.end.COdorInfo_tb_order04.txt_bold > dd").Text()
+	if price != "2,551円" {
+		log.Fatalf("price changed to %s", price)
+	}
+	// ご注文手続きへ進む
+	page.FindByID("setDelDay").Click()
+	page.Screenshot("log/07.png")
+
+	// お届け時間・お支払い方法を指定する
+	page.FindByID("imgGoNext").Click()
+	page.Screenshot("log/08.png")
+
+	// ご注文の確認へ進む
+	page.Find("#timeAndPay > div > dl.payInfo_tb2.dlvtime > dd > select").Select("2000")
+	page.Screenshot("log/09.png")
+	page.FindByID("goOrderConfirmFrom1hour04").Click()
+	page.Screenshot("log/10.png")
+
+	// 注文の確定
+	price, _ = page.FindByClass("txb").Text()
+	if price != "2,551円" {
+		log.Fatalf("price changed to %s", price)
+	}
+	//page.FindByID("goOrder").Click()
+	page.Screenshot("log/11.png")
 }
